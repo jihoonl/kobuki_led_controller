194d30b42052de151063a4a83182e40734b1794f
Jihoon Lee <jihoonlee.in@gmail.com>
2014-12-12 16:51:42 +0900

    configurable volume

--- scripts/chirp.bash
+++ scripts/chirp.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Set the volume to max
-amixer -c 0 sset PCM,0 100%
-amixer -c 0 sset Master,0 100%
+volume=${2-100}
+echo $volume
+
+amixer -c 0 sset PCM,0 $volume%
+amixer -c 0 sset Master,0 $volume%
 
 # Then moo.
 aplay $1
