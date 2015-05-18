948750c5690f99f37b25a4e9f1c24e25de8b4d08
Jihoon Lee <jihoonlee.in@gmail.com>
2015-05-18 11:01:56 +0900

    remove sound

--- src/kobuki_led_controller/sound.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def play_sound(resource_path, sound, volume=100):
-    chirp_path = subprocess.check_output(['rospack','find','kobuki_utils']).replace('\n','')
-    chirp = chirp_path + '/scripts/chirp.bash' 
-    sound_path = resource_path + '/' + sound
-    subprocess.call([chirp, sound_path, str(volume)])
